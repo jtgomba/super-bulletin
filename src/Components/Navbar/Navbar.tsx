b434acb8306f4f87e3bb7cdecdc92c399f42919f
jtgomba <jgomba@hotmail.co.uk>
2022-07-01 19:24:43 +0900

    Update Navbar.tsx

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -19,9 +19,9 @@ import MoreIcon from "@mui/icons-material/MoreVert";
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
   borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  backgroundColor: alpha(theme.palette.common.black, 0.15),
   "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
+    backgroundColor: alpha(theme.palette.common.black, 0.25),
   },
   marginRight: theme.spacing(2),
   marginLeft: 0,
@@ -181,7 +181,7 @@ export default function Navbar() {
             component="div"
             sx={{ display: { xs: "none", sm: "block" } }}
           >
-            MUI
+            Class Board
           </Typography>
           <Search>
             <SearchIconWrapper>
