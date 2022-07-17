d92d791e0022f5e1538045b79089fec46455d124
jtgomba <jgomba@hotmail.co.uk>
2022-07-17 20:05:46 +0900

    Update CardDialog.tsx

--- src/Components/ProjectBoard/BoardCard/CardDialog.tsx
+++ src/Components/ProjectBoard/BoardCard/CardDialog.tsx
@@ -6,14 +6,22 @@ import {
   DialogActions,
   Button,
   Typography,
+  Box,
+  InputBase,
+  Avatar,
+  Chip,
+  Stack,
 } from "@mui/material";
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import SegmentIcon from "@mui/icons-material/Segment";
 import NavigationIcon from "@mui/icons-material/Navigation";
-import { grey } from "@mui/material/colors";
+import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
+import HistoryIcon from "@mui/icons-material/History";
+import { blue, grey } from "@mui/material/colors";
 
 import { useGetTicketsQuery } from "../../../Utils/apis/ticketApi";
+import { borderRadius } from "@mui/system";
 
 interface DialogProps {
   open: boolean;
@@ -48,24 +56,45 @@ const CardDialog = ({ open, handleClose, cardId }: DialogProps) => {
       maxWidth={"md"}>
       <DialogTitle
         id="card-dialog-title"
-        sx={{ display: "flex", alignItems: "center" }}>
-        <NavigationIcon
-          sx={{ transform: " rotate(90deg)", marginRight: 1 }}
-          fontSize="small"
-        />
-        <Typography variant="h4">{formData?.title}</Typography>
+        sx={{
+          display: "flex",
+          alignItems: "flex-start",
+          flexDirection: "column",
+        }}>
+        <Box
+          sx={{
+            flex: 1,
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}>
+          <NavigationIcon
+            sx={{ transform: " rotate(90deg)", marginRight: 1 }}
+            fontSize="small"
+          />
+          <Typography variant="h4" gutterBottom>
+            {formData?.title}
+          </Typography>
+        </Box>
+        <Stack spacing={2} direction="row" sx={{ marginLeft: 4 }}>
+          <Chip label={formData?.priority} size="medium" />
+          <Chip label="Task" size="medium" />
+          <Chip label={formData?.status} size="medium" />
+        </Stack>
       </DialogTitle>
       <DialogContent sx={{ minWidth: "45vw" }}>
+        <Typography
+          variant="h6"
+          gutterBottom
+          sx={{ display: "flex", alignItems: "center" }}>
+          <SegmentIcon
+            sx={{ transform: "scaleX(-1)", marginRight: 1 }}
+            fontSize="small"
+          />
+          Description
+        </Typography>
         <DialogContentText id="card-dialog-description">
-          <Typography
-            variant="h6"
-            sx={{ display: "flex", alignItems: "center" }}>
-            <SegmentIcon
-              sx={{ transform: "scaleX(-1)", marginRight: 1 }}
-              fontSize="small"
-            />
-            Description
-          </Typography>
           <Typography
             sx={{
               marginLeft: 4,
@@ -76,9 +105,43 @@ const CardDialog = ({ open, handleClose, cardId }: DialogProps) => {
             }}>
             {formData && formData?.description
               ? formData?.description
-              : "This ticket is expecting a description"}
+              : "Add a description"}
           </Typography>
         </DialogContentText>
+      </DialogContent>
+      <DialogContent sx={{ minWidth: "45vw" }}>
+        <Typography
+          variant="h6"
+          gutterBottom
+          sx={{ display: "flex", alignItems: "center" }}>
+          <QuestionAnswerIcon sx={{ marginRight: 1 }} fontSize="small" />
+          Comments
+        </Typography>
+        <Box sx={{ display: "flex", flexDirection: "row" }}>
+          <Avatar sx={{ bgcolor: blue[500], marginRight: "0.5rem" }}>JG</Avatar>
+          <InputBase
+            sx={{
+              border: `1px solid ${grey[300]}`,
+              padding: "5px",
+              flex: "1",
+            }}
+            placeholder="Write a comment"
+            /*               value={ticketTitle}
+              onChange={(e) => setTicketTitle(e.target.value)} */
+          />
+        </Box>
+      </DialogContent>
+      <DialogContent sx={{ minWidth: "45vw" }}>
+        <Typography
+          variant="h6"
+          gutterBottom
+          sx={{ display: "flex", alignItems: "center" }}>
+          <HistoryIcon
+            sx={{ transform: "scaleX(-1)", marginRight: 1 }}
+            fontSize="small"
+          />
+          History
+        </Typography>
       </DialogContent>
       <DialogActions>
         <Button
