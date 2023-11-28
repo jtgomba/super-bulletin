bb50d1b674f74fc16423e99e00f6a4f28f2f94f6
jtgomba <jgomba@hotmail.co.uk>
2023-11-28 23:44:59 +0900

    style: prettier

--- src/app/(LandingPage)/page.tsx
+++ src/app/(LandingPage)/page.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button';
 export default function LandingPage() {
   return (
     <div className='flex flex-col items-center justify-center'>
-      <div className='font-heading flex flex-col items-center justify-center'>
+      <div className='flex flex-col items-center justify-center font-heading'>
         <div className='mb-4 flex items-center rounded-full border bg-amber-100 p-4 uppercase text-amber-700 shadow-sm'>
           <Medal />
           No 1 task Management
@@ -18,7 +18,7 @@ export default function LandingPage() {
           work forward
         </div>
       </div>
-      <div className='font-poppins mx-auto mt-4 max-w-xs text-center text-sm text-neutral-400 md:max-w-2xl md:text-xl'>
+      <div className='mx-auto mt-4 max-w-xs text-center font-poppins text-sm text-neutral-400 md:max-w-2xl md:text-xl'>
         Colloaborate, manage projects, and reach new productivity peaks. From
         high risees to the home office, the way your team works is unique -
         accomplish it all with SuperBulletin.
