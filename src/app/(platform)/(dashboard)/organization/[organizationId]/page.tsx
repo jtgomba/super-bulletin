19f112a1fec36aa5b6dd1f755b6b82558d593d57
jtgomba <jgomba@hotmail.co.uk>
2023-12-12 17:57:22 +0900

    .

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,20 +1,5 @@
-import { db } from '@/lib/db';
-
-import { Form } from './form';
-
-const OrganizationPage = async () => {
-  const boards = await db.board.findMany();
-
-  return (
-    <div className='flex flex-col space-y-4'>
-      <Form />
-      <div className='space-y-2'>
-        {boards.map((board) => (
-          <div key={board.id}>Board Title: {board.title}</div>
-        ))}
-      </div>
-    </div>
-  );
+const OrganizationIdPage = async () => {
+  return <div className='flex flex-col space-y-4'></div>;
 };
 
-export default OrganizationPage;
+export default OrganizationIdPage;
