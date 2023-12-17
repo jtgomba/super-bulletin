767e8069028b5d27c2496702800cd18c89e49ffb
jtgomba <jgomba@hotmail.co.uk>
2023-12-17 19:13:19 +0900

    feat: added board list skeleton

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,18 @@
+import { Separator } from '@/components/ui/separator';
+
+import { BoardList } from './_components/board-list';
+import { Info } from './_components/info';
+
 const OrganizationIdPage = async () => {
-  return <div className='flex flex-col space-y-4'></div>;
+  return (
+    <div className='mb-20 w-full'>
+      <Info />
+      <Separator className='my-4' />
+      <div className='px-2 md:px-4'>
+        <BoardList />
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
