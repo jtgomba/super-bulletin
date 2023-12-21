d309e399e8d7bb7d6145c765b3e2ce73f102e734
jtgomba <jgomba@hotmail.co.uk>
2023-12-22 01:09:26 +0900

    feat: added editable header for card modal

--- src/components/modals/card-modal/index.tsx
+++ src/components/modals/card-modal/index.tsx
@@ -1,17 +1,30 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
+
 import { Dialog, DialogContent } from '@/components/ui/dialog';
 import { useCardModal } from '@/hooks/use-card-modal';
+import { fetcher } from '@/lib/fetcher';
+import { CardWithList } from '@/types';
+
+import { Header } from './header';
 
 export const CardModal = () => {
   const { id, isOpen, onClose } = useCardModal((state) => state);
+
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ['card', id],
+    queryFn: () => fetcher(`/api/cards/${id}`),
+  });
 
   return (
     <Dialog
       open={isOpen}
       onOpenChange={onClose}
     >
-      <DialogContent>I am a modal</DialogContent>
+      <DialogContent>
+        {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+      </DialogContent>
     </Dialog>
   );
 };
